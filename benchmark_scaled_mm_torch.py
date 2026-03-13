4fd8aa140c9a25f311932be6c43721dc2d01f797
woctordho <woctordho@outlook.com>
2026-03-13 08:48:26 +0800

    Add hipBLASLt kernel

---
+++ benchmark_scaled_mm_torch.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+
+import gc
+
+import torch
+import triton
+
+from kernel.naive import scaled_mm_naive
+
+scaled_mm_naive_compiled = torch.compile(scaled_mm_naive, fullgraph=True, dynamic=False, mode="max-autotune")
+
+providers = {
+    "torch": scaled_mm_naive,
+    "torch_compiled": scaled_mm_naive_compiled,
+}
+provider_names = list(providers)
+
+
+@triton.testing.perf_report(
+    [
+        triton.testing.Benchmark(
+            x_names=["N"],
+            x_vals=[256, 512, 1024, 2048, 4096, 8192],
+            line_arg="provider",
+            line_vals=provider_names,
+            line_names=provider_names,
+            ylabel="GFLOPS",
+            plot_name="scaled_mm_torch",
+            args={},
+        )
+    ]
+)
+def benchmark(N, provider):
+    print("N", N, "provider", provider, "begin")
+    gc.collect()
+    torch.cuda.empty_cache()
+
+    device = "cuda"
+    a_dtype = torch.float16
+    b_dtype = torch.float16
+    out_dtype = torch.float16
+
+    a = torch.randn((N, N), device=device, dtype=torch.float32).to(a_dtype)
+    b = torch.randn((N, N), device=device, dtype=torch.float32).to(b_dtype)
+    scale = None
+    bias = None
+
+    quantiles = [0.5, 0.2, 0.8]
+    ms, min_ms, max_ms = triton.testing.do_bench(lambda: providers[provider](a, b, scale, bias, out_dtype), warmup=100, rep=1000, quantiles=quantiles)
+
+    perf = lambda ms: 2 * N**3 / ms * 1e-6
+    print("N", N, "provider", provider, "end", perf(ms))
+    return perf(ms), perf(max_ms), perf(min_ms)
+
+
+if __name__ == "__main__":
+    with torch.inference_mode():
+        benchmark.run(print_data=True, save_path="./")
