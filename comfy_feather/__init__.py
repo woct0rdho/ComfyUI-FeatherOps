f351c9d4801e53d8614a0987ec887a647940fbb9
woctordho <woctordho@outlook.com>
2026-03-20 15:38:12 +0800

    Add FeatherCLIPTextEncodePadded

--- comfy_feather/__init__.py
+++ comfy_feather/__init__.py
@@ -1,9 +1,11 @@
-from .loader import FeatherUNetLoader
+from .loader import FeatherCLIPTextEncodePadded, FeatherUNetLoader
 
 NODE_CLASS_MAPPINGS = {
     "FeatherUNetLoader": FeatherUNetLoader,
+    "FeatherCLIPTextEncodePadded": FeatherCLIPTextEncodePadded,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "FeatherUNetLoader": "Load Diffusion Model (Feather)",
+    "FeatherCLIPTextEncodePadded": "Text Encode Padded (Feather)",
 }
