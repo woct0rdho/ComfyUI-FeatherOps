d662b1bd6190d41f6a11f1adff84e8400540dafe
woctordho <woctordho@outlook.com>
2026-05-18 20:43:21 +0800

    Update hipBLASLt benchmark script

--- kernel/naive.py
+++ kernel/naive.py
@@ -10,11 +10,14 @@ def scaled_mm_naive(
     bias: Optional[torch.Tensor],
     out_dtype: torch.dtype,
     b_prepacked: bool = False,
+    bias_dim: int = 1,
 ) -> torch.Tensor:
     if b_prepacked:
         # (K/16, N, 16) -> (K, N)
         k, N, _ = b.shape
         b = b.permute(0, 2, 1).reshape(k * 16, N)
+    if bias_dim not in {0, 1}:
+        raise RuntimeError(f"bias_dim must be 0 or 1, got {bias_dim}")
 
     mm_dtype = torch.float16
     a = a.to(mm_dtype)
@@ -22,7 +25,15 @@ def scaled_mm_naive(
     c = a @ b
     c = c.to(out_dtype)
     if scale is not None:
-        c *= scale.to(c.dtype)
+        scale = scale.to(c.dtype)
+        if bias_dim == 0:
+            c *= scale[:, None]
+        else:
+            c *= scale[None, :]
     if bias is not None:
-        c += bias.to(c.dtype)
+        bias = bias.to(c.dtype)
+        if bias_dim == 0:
+            c += bias[:, None]
+        else:
+            c += bias[None, :]
     return c
